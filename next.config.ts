16faaff54bdb749e1840bf01f293186a9ba6a7a5
alam-gir <info.alamgirhussain@gamil.com>
2025-10-04 02:37:45 +0600

    host added in the next config for next IMAGE.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pub-df8554bc8acd468e9312f01236ca95f5.r2.dev',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
